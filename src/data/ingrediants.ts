d556b3522365406a4dfcb62ff5c8a7da00b16c2b
Mihailo <mihailoless@gmail.com>
2025-12-14 14:30:08 +0100

    Finished nutritions data

---
+++ src/data/ingrediants.ts
@@ -0,0 +1,17 @@
+export const ingredInfo = [
+  {
+    ingrediant: '2-3 large eggs'
+  },
+  {
+    ingrediant: 'Salt, to taste'
+  },
+  {
+    ingrediant: 'Pepper, to taste'
+  },
+  {
+    ingrediant: '1 tablespoon of butter or oil'
+  },
+  {
+    ingrediant: 'Optional fillings: cheese, diced vegetables, cooked meats, herbs '
+  }
+]
---
+++ src/data/nutrition.ts
@@ -0,0 +1,34 @@
+export const nutrition = [
+  {
+    name: 'Calories',
+    amout:
+    {
+      value: 277,
+      unit: 'kcal'
+    }
+  },
+  {
+    name: 'Carbs',
+    amout:
+    {
+      value: 0,
+      unit: 'g'
+    }
+  },
+  {
+    name: 'Protein',
+    amout:
+    {
+      value: 20,
+      unit: 'g'
+    }
+  },
+  {
+    name: 'Fat',
+    amout:
+    {
+      value: 22,
+      unit: 'g'
+    }
+  },
+]
