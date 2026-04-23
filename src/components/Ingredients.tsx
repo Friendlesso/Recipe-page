a911b3f83db06b9ca89f5090a636d84bd0b3bfcc
Mihailo <mihailoless@gmail.com>
2026-04-23 23:20:32 +0200

    Update Ingredients.tsx

--- src/components/Ingredients.tsx
+++ src/components/Ingredients.tsx
@@ -3,7 +3,7 @@ import { ingredInfo } from "../data/ingrediants";
 export function Ingredients() {
   return(
     <section className="border-b-2 border-(--border-color) mb-5 pb-3">
-      <h3 className="text-(--text-brown) font-[YoungSerif] text-2xl mb-3">Ingredients</h3>
+      <h2 className="text-(--text-brown) font-[YoungSerif] text-2xl mb-3">Ingredients</h2>
       <ul className="list-disc list-inside px-2 marker:text-xs marker:text-(--text-brown)">
         {ingredInfo.map(({ingrediant})=> (
           <li key={ingrediant} className="text-(--light-text-color) font-[OutfitNormal] mb-4">{ingrediant}</li>
