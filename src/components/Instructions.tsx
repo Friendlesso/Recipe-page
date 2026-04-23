87648c3930f4df176eeec9aaeefebd3b18c0903f
Mihailo <mihailoless@gmail.com>
2026-04-23 23:20:56 +0200

    Update Instructions.tsx

--- src/components/Instructions.tsx
+++ src/components/Instructions.tsx
@@ -3,7 +3,7 @@ import { instructions } from "../data/instructions";
 export function Instructions() {
   return (
     <section className="border-b-2 border-(--border-color) mb-5 pb-3">
-      <h3 className="text-(--text-brown) font-[YoungSerif] text-2xl mb-3">Instructions</h3>
+      <h2 className="text-(--text-brown) font-[YoungSerif] text-2xl mb-3">Instructions</h2>
       <ol className="list-decimal list-outside px-6 marker:text-(--text-brown) marker:font-bold">
         {instructions.map(({ name, instruction }) => (
           <li className="text-(--light-text-color) font-[OutfitNormal] mb-4" key={name}>
