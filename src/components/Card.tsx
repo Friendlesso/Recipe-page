763ed766c499eb2d24a3f82a06c36110e889c84a
Mihailo <mihailoless@gmail.com>
2026-04-23 23:27:37 +0200

    Update Card.tsx

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -7,13 +7,13 @@ import { Preparation } from "./Preparation";
 
 export function Card() {
   return(
-    <div className="flex flex-col max-w-[100vw] w-4xl bg-white md:rounded-[1.250rem] p-8">
+    <main className="flex flex-col max-w-[100vw] w-4xl bg-white md:rounded-[1.250rem] p-8">
       <FoodImage />
       <Description />
       <Preparation />
       <Ingredients />
       <Instructions />
       <Nutrition />
-    </div>
+    </main>
   )
 }
